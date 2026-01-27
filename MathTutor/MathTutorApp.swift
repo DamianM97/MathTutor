ed6fb83433b710f8831e561218c514585f8d8bfb
Damian Martinez <deej07506@gmail.com>
2026-01-27 10:28:58 -0800

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by MARTINEZ, DAMIAN on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
